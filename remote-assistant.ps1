c4ccbe881e57de36c0a7194e229ab54b1a071118
HeiligerMax <49809589+HeiligerMax@users.noreply.github.com>
2019-05-29 07:48:08 +0200

    use WPF

--- remote-assistant.ps1
+++ remote-assistant.ps1
@@ -1,83 +1,73 @@
 #https://github.com/HeiligerMax
 #Licensed under GNU General Public License v3.0
 
-#Integrate Windows Forms
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.Application]::EnableVisualStyles()
-
-#Get control over the console
+#Get control over the Powershell window
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
-
-#---GUI---
-$Form = New-Object System.Windows.Forms.Form
-$Form.ClientSize = "300,120"
-$Form.Text = "Remote Assistant"
-$Form.FormBorderStyle = "FixedSingle"
-$Form.MaximizeBox = $false
-$Form.StartPosition = "CenterScreen"
-$Form.KeyPreview = $True
-$Form.Add_KeyDown({if($_.KeyCode -eq 'Enter'){$Button.PerformClick()}})
-
-$InputField = New-Object System.Windows.Forms.ComboBox
-$InputField.Location = New-Object System.Drawing.Point(5,5)
-$InputField.Size = New-Object System.Drawing.Size(290,30)
-$InputField.Font = "Microsoft Sans Serif,12"
-$InputField.Text = ""
-$InputField.MaxDropDownItems = "25"
-
-$Button = New-Object System.Windows.Forms.Button
-$Button.Location = New-Object System.Drawing.Point(5,40)
-$Button.Size = New-Object System.Drawing.Size(290,30)
-$Button.Font = "Microsoft Sans Serif,12"
-$Button.Text = "Engage"
-$Button.Add_Click({letsgo})
-
-$Status = New-Object System.Windows.Forms.Label
-$Status.Location = New-Object System.Drawing.Point(5,75)
-$Status.Size = New-Object System.Drawing.Size(290,40)
-$Status.TextAlign = "MiddleCenter"
-$Status.Font = "Microsoft Sans Serif,12"
-$Status.Text = "Ready"
-
-$Form.Controls.AddRange(@($InputField,$Button,$Status))
-
-#---function---
-function letsgo{
+###
+#Add WPF
+Add-Type -AssemblyName PresentationFramework
+[xml]$xaml = @"
+<Window x:Name="Window"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Remote-Assistent" Width="300" Height="200" WindowStartupLocation="CenterScreen" FontFamily="Microsoft Sans Serif" FontSize="16" SizeToContent="Height" ResizeMode="CanMinimize" MinWidth="300" MinHeight="100">
+    <Grid>
+        <ComboBox x:Name="InputField" Margin="10,10,10,0" Padding="5" IsEditable="True" VerticalAlignment="Top" ToolTip="The whole or part of Computername"/>
+        <Button x:Name="Button" Content="Search and connect" Margin="10,50,10,0" Padding="5" VerticalAlignment="Top" IsDefault="True" ToolTip="Search for the Computername in AD"/>
+        <TextBox x:Name="OutputField" Margin="10,90,10,10" Padding="5" TextWrapping="Wrap" Text="Ready" HorizontalContentAlignment="Center" IsReadOnly="True" ToolTip="https://github.com/HeiligerMax"/>
+    </Grid>
+</Window>
+"@
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($reader)
+###
+$InputField = $window.FindName("InputField")
+$Button = $window.FindName("Button")
+$OutputField = $window.FindName("OutputField")
+$Button.Add_Click{(offerra)}
+###
+function offerra{
     $Search = $InputField.Text
-    $Status.Text = "Searching for PC"
+    $OutputField.Text = "Suche nach APC"
     $PC = Get-ADComputer -Filter "Name -like '*$Search*'" | Select-Object -ExpandProperty Name
 
     if($PC.Count -gt 1){
-        $Status.Text = "More than one PC found"
-        $PC | foreach{$InputField.Items.Add($_)}
-        $InputField.Text = "Please select PC"
+        $OutputField.Text += "`r`nMore than one Computer found"
+        $PC | ForEach-Object{$InputField.Items.Add($_)}
+        $InputField.Text = "Please select Computer"
+        $InputField.IsDropDownOpen = $true
         return
-    }elseif($PC -eq $null){
-        $Status.Text = "No PC found. Connect anyway. (IP-Address?)"
-        msra.exe /offerra $Search
+    }elseif($null -eq $PC){
+        $OutputField.Text += "`r`nNo Computer found."
+        $BoxAntwort = [System.Windows.MessageBox]::Show("No Computer found!`r`nConnect to $Search anyway?",":(","YesNo","Error")
+        if($BoxAntwort -eq "Yes"){
+            $OutputField.Text += "`r`Connecting"
+            msra.exe /offerra $Search
+        }
+        $OutputField.Text = "Ready"
         return
     }
 
     $InputField.Text = "$PC"
-    $Status.Text = "Testing Connection"
+    $OutputField.Text += "`r`nTesting connection"
     if(Test-NetConnection -ComputerName $PC -InformationLevel Quiet){
-        $Status.Text = "Connecting..."
+        $OutputField.Text += "`r`Connecting`r`n"
         msra.exe /offerra $PC
         $InputField.Items.Clear()
         $InputField.Text =""
-        $Status.Text = "Ready"
+        $OutputField.Text = "Ready"
     }else{
-        $Status.Text = "$PC not reachable!"
+        $OutputField.Text += "`r`n$PC not reachable!"
     }
 }
-
-#Hide Console when GUI appears and bring it back if GUI gets closed
+###
+#Hide Powershell when GUI appears and bring it back if GUI gets closed
 $consolePtr = [Console.Window]::GetConsoleWindow()
 [Console.Window]::ShowWindow($consolePtr, 0)
-$Form.ShowDialog()
+$window.ShowDialog() | Out-Null
 [Console.Window]::ShowWindow($consolePtr, 4)
